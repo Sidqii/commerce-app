d315f2d679f304e6f3628f82de45f96735531f62
Sidqii <sidqimhmmd412@gmail.com>
2025-12-17 12:21:08 +0700

    screenshots

--- lib/app/utils/config/env_dev.dart
+++ lib/app/utils/config/env_dev.dart
@@ -2,5 +2,5 @@ import 'package:suaka_niaga/app/utils/config/app_env.dart';
 
 class DevEnvironment implements AppEnv {
   @override
-  String get url => 'https://9269d33efc36.ngrok-free.app';
+  String get url => 'https://5820552f9b81.ngrok-free.app';
 }
